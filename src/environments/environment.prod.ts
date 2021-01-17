1093e749f7e2e144b4ceb79c4682cd19c3082d0a
nizam9 <nizamuddin407@gmail.com>
2021-01-17 23:52:49 +0530

    modified env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SECRET_KEY: ${{ secrets.API_SECRET }},
+  SECRET_KEY: '82a8dfb9f103785ed5e473c863f6d13f',
   ENDPOINT: 'https://developers.zomato.com/api/v2.1',
 };
 
